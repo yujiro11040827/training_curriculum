1ee6bd986b66ef22e813a7b2a4adcc9a7a20bbe0
yujiro11040827 <71250516+yujiro11040827@users.noreply.github.com>
2020-10-25 18:26:48 +0900

    Revert "Secound training"

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,19 +34,7 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      
-      wday_num = Date.today.wday
-      if wday_num >= 7
-        wday_num = wday_num - 7
-      end
-
-      7.times do |x|
-        today_plans = []
-        plan = plans.map do |plan|
-          today_plans.push(plan.plan) if plan.date == @todays_date + x
-        end
-        
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday:""}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,4 @@
 class Plan < ApplicationRecord
   validates :plan, presence: true
   validates :date, presence: true
-  validates :calendars, presence: true
 end
